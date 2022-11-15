0b0460b20142097d7c391e3ca9cb239835783f78
Aqsa Aqeel <aqsa.aqeel17@example.com>
2022-11-16 02:09:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import {Routes,Route} from "react-router-dom"
+import {Login,Landing, Signup,Home} from "./features"
+import { Navbar,Footer } from './component';
+import  Todo from './component/Todo/Todo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+     <Routes>
+       <Route path="/" element={<Landing />} />
+       <Route path="login" element={<Login />} />
+       <Route path="signup" element={<Signup />} />
+       <Route path="/home" element={<Home />} />
+       <Route path="/todo" element={<Todo />} />
+     </Routes>
+     <Footer />
     </div>
   );
 }
