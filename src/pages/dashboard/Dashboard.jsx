3b80df610bd2c6e995daa7a1ccb0b550f93e8bfa
Aqsa Aqeel <aqsa.aqeel17@example.com>
2023-03-07 21:13:32 +0530

    refactor: changed Link tag

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -13,10 +13,10 @@ export const Dashboard = () => {
           <div className="row-span-1 text-2xl subheading text-center">Pick a language</div>
           <div className="row-span-9 subheading text-center flex flex-wrap gap-10 justify-center p-10">
             <Link to="/progress/javascript"><div className="language cursor-pointer hover:scale-125 bg-blue-400 w-48 h-24 flex justify-center items-center rounded-lg text-zinc-100">JavaScript</div></Link>
-            <a href="/"><div className="language cursor-pointer hover:scale-125 bg-blue-400 w-48 h-24 flex justify-center items-center rounded-lg text-zinc-100">Python</div></a>
-            <a href="/"><div className="language cursor-pointer hover:scale-125 bg-slate-400 w-48 h-24 flex justify-center items-center rounded-lg text-zinc-100">C</div></a>
-            <a href="/"><div className="language cursor-pointer hover:scale-125 bg-slate-400 w-48 h-24 flex justify-center items-center rounded-lg text-zinc-100">HTML</div></a>
-            <a href="/"><div className="language cursor-pointer hover:scale-125 bg-slate-400 w-48 h-24 flex justify-center items-center rounded-lg text-zinc-100">CSS</div></a>
+            <Link to="/"><div className="language cursor-pointer hover:scale-125 bg-blue-400 w-48 h-24 flex justify-center items-center rounded-lg text-zinc-100">Python</div></Link>
+            <Link to="/"><div className="language cursor-pointer hover:scale-125 bg-slate-400 w-48 h-24 flex justify-center items-center rounded-lg text-zinc-100">C</div></Link>
+            <Link to="/"><div className="language cursor-pointer hover:scale-125 bg-slate-400 w-48 h-24 flex justify-center items-center rounded-lg text-zinc-100">HTML</div></Link>
+            <Link to="/"><div className="language cursor-pointer hover:scale-125 bg-slate-400 w-48 h-24 flex justify-center items-center rounded-lg text-zinc-100">CSS</div></Link>
           </div>
         </div>
       </div>
