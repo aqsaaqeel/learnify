3712f7057660dd9734892d83f84dbb7a32acd9bd
Aqsa Aqeel <aqsa.aqeel17@example.com>
2023-03-10 01:34:43 +0530

    refactor: memory game logistics

--- src/component/winning-modal/WinningModal.jsx
+++ src/component/winning-modal/WinningModal.jsx
@@ -1,11 +1,43 @@
 export const WinningModal = (props) => {
-    return (
-      <div className="modal" style={{display: props.show ? "block" : "none"}}>
-        <div className="modal-content">
-          <h1 className="text-4xl ">You won by answering everything correctly, Yayyy!!!</h1>
-          <button onClick={props.onClose}>Close</button>
+  return (
+    <div
+      class="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div class="sm:flex sm:items-start">
+                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <h3
+                    class="text-base font-semibold leading-6 text-gray-900"
+                    id="modal-title"
+                  >
+                    Yayyyy you answered everything correctly and wonnn!!!
+                  </h3>
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-500">{props.question}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+ 
+            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button
+                type="button"
+                class="inline-flex w-full justify-center rounded-md bg-blue-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+              >
+                Take me to next level
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+};
