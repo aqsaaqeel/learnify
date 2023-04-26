6ad57b4ee787cfddad1226e3cfe177f9eb3e7979
Mahak491 <shreemahak01@gmail.com>
2023-04-26 08:50:26 +0530

    leveltwo java and cpp array topic added

--- src/contents/level2/LeveTwoCpp.jsx
+++ src/contents/level2/LeveTwoCpp.jsx
@@ -1,79 +1,51 @@
-const codeSnippet = `int x = 10; string y = "hello";
-float height = 1.75;
-char initial = 'J';   `;
-export const LevelTwoCpp = () => {
+const codeSnippet = `int arr[5] = {10, 20, 30, 40, 50}; 
+float floatArr[3] = {20.008, 30.5, 40.2}; 
+char charArr[4] = {'J', 'o', 'h', 'n'};`;
+export const LevelTwoCPlusPlus = () => {
   return (
     <div className="installation-info flex-1 flex flex-col items-start font-bold bg-slate-200 rounded-md p-10 mx-5 h-auto">
-      <h1 className="text-4xl">What are variables?</h1>
+      <iframe
+        width="420"
+        height="315"
+        src=""
+        className="p-5"
+        title="cpp-arrays"
+      ></iframe>
+      <h1 className="text-4xl">
+        Arrays
+      </h1>
       <p className="inline-block font-thin text-left items-start justify-start py-5">
-        In C++, a variable is a named storage location in memory that holds a
-        value of a specific data type. Like C, a variable in C++ has a data type
-        that determines the type of data that can be stored in it, and the value
-        stored in a variable can be changed during the execution of the program.
-        C++ also supports pointers, which are variables that store memory
-        addresses.{" "}
+        In C++, an array is a collection of elements of the same type, stored
+        in a contiguous memory location. The size and type of the array are
+        fixed at compile time.{" "}
         <span className="text-blue-600 font-bold">
-          To declare a variable in C++, you specify its type and name. C++ also
-          supports reference variables, which are variables that refer to other
-          variables.
+          To declare an array in C++, you specify the type of elements it
+          contains and the number of elements it can hold. You can also
+          initialize an array with a list of values enclosed in braces.
         </span>
       </p>
-      <h2 className="text-2xl">Important things to know about variables</h2>
+      <h2 className="text-2xl">Important things to know about arrays</h2>
       <ol className="inline-block font-thin text-left items-start justify-start py-1 list-decimal">
         <li className="p-5">
-          In C++, variables must be declared with a data type before they can be
-          used.
+          Arrays are used to store multiple values of the same type in C++
+          programs.
         </li>
         <li className="p-5">
-          The data type determines the range of values that the variable can
-          store and the operations that can be performed on it.
+          An array must be declared before it can be used. The declaration
+          specifies the type of elements the array contains and the number of
+          elements it can hold.
         </li>
         <li className="p-5">
-          Variables can be initialized with a value when they are declared or at
-          a later point in the program. If a variable is not initialized, its
-          initial value is undefined.
+          The elements of an array can be accessed using an index. Array indexes
+          start from zero in C++.
         </li>
         <li className="p-5">
-          The scope of a variable in C++ is the part of the program where the
-          variable can be accessed. Variables declared inside a block of code
-          have block scope, while variables declared outside of any block of
-          code have global scope.
+          In C++, there are several types of arrays available, including
+          arrays of integers, floating-point numbers, characters, and more.
         </li>
         <li className="p-5">
-          The lifetime of a variable in C++ is the period of time during which
-          the variable exists in memory. Automatic variables are created when a
-          block of code is entered and destroyed when the block of code is
-          exited, while dynamically allocated variables exist until they are
-          explicitly deallocated.
-        </li>
-        <li className="p-5">
-          Reference variables in C++ are aliases for other variables. They are
-          declared with the & symbol and allow multiple names to refer to the
-          same value in memory.
-        </li>
-        <li className="p-5">
-          Pointer variables in C++ store memory addresses instead of values.
-          They are declared with the * symbol and can be used to access the
-          value stored at a particular memory address.
-        </li>
-        <li className="p-5">
-          Constants in C++ are variables that cannot be modified once they have
-          been initialized. They are declared with the const keyword and are
-          often used to define values that are used throughout a program.
-        </li>
-        <li className="p-5">
-          C++ has several storage classes that determine the lifetime and
-          visibility of a variable, including auto, static, register, and
-          extern.
-        </li>
-        <li className="p-5">
-          Typecasting is the process of converting a variable from one data type
-          to another. This can be done explicitly with the use of casting
-          operators, such as (int) or (float).
-        </li>
-        <li>
-          To declare a variable in C++, you specify its type and name. You can
-          also initialize a variable at the same time you declare it, like this:
+          Arrays can be initialized with a list of values enclosed in braces.
+          For Example,
         </li>
         <div className="bg-gray-100 p-4 rounded-md my-7">
           <pre>
@@ -86,11 +58,18 @@ export const LevelTwoCpp = () => {
             Copy Code
           </button>
         </div>
-        <li className="p-5">
-          C++ allows you to organize your code using namespaces, which provide a
-          way to group related variables and functions together. Namespaces can
-          help prevent naming conflicts and make your code more modular and
-          maintainable.
+        <li>
+          The size of an array in C++ is fixed at compile time and cannot be
+          changed at runtime.
+        </li>
+        <li>
+          Arrays in C++ can be passed as arguments to functions, returned from
+          functions, and used in expressions.
+        </li>
+        <li>
+          C++ provides several functions to work with arrays, such as the size
+          function to get the number of elements in an array and the sort
+          function to sort the elements of an array.
         </li>
       </ol>
     </div>
